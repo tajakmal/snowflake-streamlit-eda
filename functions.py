2e09dee395df843715b54e6ebb5ed3fd2b3d55f8
tajakmal <tajwar.akmal@gmail.com>
2024-01-02 21:45:50 -0500

    Refactor df_info function to improve column processing

--- functions.py
+++ functions.py
@@ -3,26 +3,31 @@ import pandas as pd
 import streamlit as st
 
 def df_info(df):
+    # Ensure the column names do not have spaces for easier processing
     df.columns = df.columns.str.replace(' ', '_')
-    buffer = io.StringIO() 
-    df.info(buf=buffer)
-    s = buffer.getvalue() 
-
-    df_info = s.split('\n')
-
-    counts = []
+    
+    # Initialize lists to store column information
     names = []
     nn_count = []
     dtype = []
-    for i in range(5, len(df_info)-3):
-        line = df_info[i].split()
-        counts.append(line[0])
-        names.append(line[1])
-        nn_count.append(line[2])
-        dtype.append(line[4])
+    unique_non_null_count = []
 
-    df_info_dataframe = pd.DataFrame(data = {'#':counts, 'Column':names, 'Non-Null Count':nn_count, 'Data Type':dtype})
-    return df_info_dataframe.drop('#', axis = 1)
+    # Iterate through each column to gather information
+    for col in df.columns:
+        names.append(col)
+        nn_count.append(df[col].notnull().sum())
+        dtype.append(df[col].dtype)
+        unique_non_null_count.append(df[col].nunique())
+
+    # Creating DataFrame to display the information
+    df_info_dataframe = pd.DataFrame({
+        'Column': names, 
+        'Non-Null Count': nn_count, 
+        'Unique Non-Null Count': unique_non_null_count, 
+        'Data Type': dtype
+    })
+
+    return df_info_dataframe
 
 def df_isnull(df):
     res = pd.DataFrame(df.isnull().sum()).reset_index()
